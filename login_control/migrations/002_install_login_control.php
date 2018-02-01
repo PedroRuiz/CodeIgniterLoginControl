8c056f33b6d75c01daffc9bd512d991bd2e69462
Pedro Ruiz Hidalgo <6648808+PedroRuiz@users.noreply.github.com>
2018-02-01 14:23:17 +0100

    Update 002_install_login_control.php
    tables a table

--- login_control/migrations/002_install_login_control.php
+++ login_control/migrations/002_install_login_control.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Migration_install_login_control extends CI_Migration {
-	private $tables;
+	private $table;
 
 	public function __construct() {
 		parent::__construct();
